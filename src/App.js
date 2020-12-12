c0ac88113892c9d45f7199f8efae4ffebcb61233
Haloren <mckennakyler@gmail.com>
2020-12-11 20:30:05 -0700

    testing if api is working and setting up layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 class App extends React.Component {
+  
+  componentDidMount() {
+    fetch('http://localhost:3001/users')
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Wether Weather</h1>
+        <div>
+          <h3 className="day">Tuesday</h3>
+          <h3 className="date">Dec 8 </h3>
+          <h3 className="time">12:23 am</h3>
+          <h2 className="location">Jumanji, FI</h2>
+          <h1 className="temp">46°</h1>
+        </div>
+        <br></br>
+        <div>
+          <div>
+            <h3 className="nextdays">Wednesday</h3>
+            <h3 className="nextdaystemp">46°</h3>
+          </div>
+          <div>
+            <h3 className="nextdays">Thursday</h3>
+            <h3 className="nextdaystemp">49°</h3>
+          </div>
+          <div>
+            <h3 className="nextdays">Friday</h3>
+            <h3 className="nextdaystemp">48°</h3>
+          </div>
+        </div>
+        <br></br>
+        <footer className="copyright"><small> &copy;2020WetherWeather </small></footer>
       </div>
     );
   }
