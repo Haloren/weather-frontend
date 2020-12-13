9b9d59baee6d47e5278b8c4c91bf887133a6eb04
Haloren <mckennakyler@gmail.com>
2020-12-12 23:04:57 -0700

    set up create user form input

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 class UserForm extends React.Component {
 
+    state = {email: ''}
+
+    handleOnChange = (event) => {
+        // debugger;
+        this.setState({
+            // email: event.target.value
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmit = () => {
+        
+    }
+
     render() {
         return (
             <div>
-                UserForm
+                <form className="input-container" onSubmit={this.handleOnSubmit}>
+                    <input className="input-text" type="email" value={this.state.email} name="email" onChange={this.handleOnChange} required></input>
+                    <span className="floating-label">Enter your Email</span>
+                    <input className="input-btn" type="submit" value="Login"></input>
+                </form>
             </div>
         )
     }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Users = (props) => {
-    // console.log(props)
     return (
         <div>
             {props.users.map(user => 
